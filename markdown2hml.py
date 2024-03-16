f3e2964a342972519eafe1d8cc12f894019e0eae
olhamish <olgami1402@gmail.com>
2024-03-16 02:40:46 +0200

    remove unnecessary for loop

--- markdown2hml.py
+++ markdown2hml.py
@@ -12,8 +12,7 @@ def convert_markdown_to_html(input_file, output_file=None):
                 with open(output_file, 'w') as f_out:
                     f_out.write(html_result)
             else:
-                for paragraph in html_result:
-                    print(paragraph)
+                print(html_result)
     except FileNotFoundError:
         print(f"Error: Input file '{input_file}' not found.", file=sys.stderr)
         sys.exit(1)
